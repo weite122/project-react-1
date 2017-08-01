5cb7d28943f172b91d65d0aab8ff7bc7cdeef816
weite122 <949193531@qq.com>
2017-08-02 01:03:40 +0800

    使用webpack-dev-server

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 import _ from 'lodash';
-import j from 'jquery'
-import foo from './foo'
+import j from 'jquery';
+import foo from './foo';
 
 function component () {
   var element = j('<div></div>')
 
   /* lodash is required for the next line to work */
-  element.html(_.join(['Hello','webpack'], ' '));
+  element.html(_.join(['Hi','webpack'], ' '));
 
   return element.get(0);
 }
