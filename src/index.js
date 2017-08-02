dd06398309f65dd7b711d159077700b08c4f8a61
weite122 <949193531@qq.com>
2017-08-03 02:08:58 +0800

    Component

--- src/index.js
+++ src/index.js
@@ -3,17 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-function tick(){
-  const element = (
-    <div>
-      <h1>Hello,world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(
-    element,
-    document.getElementById('root')
-  );
-}
+import Welcome from './Welcome'
 
-setInterval(tick,1000)
+ReactDOM.render(
+  <Welcome/>,
+  document.getElementById('root')
+)
+
