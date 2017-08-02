c37706ff66e2a5bcddb524cdb9044c7f98ed93b6
weite122 <949193531@qq.com>
2017-08-03 02:49:09 +0800

    props-2

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,7 +1,5 @@
 import React from 'react';
-class Welcome extends React.Component{
-  render(){
-    return <h1>Hello, {this.props.name}</h1>;
-  }
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
 }
 export default Welcome
