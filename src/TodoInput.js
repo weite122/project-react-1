2c0891c5a4346f4f2935f4e188cd32ec6612a8f7
weite122 <949193531@qq.com>
2017-08-04 03:34:30 +0800

    this.sumbit.bind(this)

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class TodoInput extends Component {
   render(){
     return <input type="text" defaultValue={this.props.content}
-        onKeyPress={this.sumbit}/>
+        onKeyPress={this.sumbit.bind(this)}/>
   }
   sumbit(e){
       if(e.key === 'Enter'){
