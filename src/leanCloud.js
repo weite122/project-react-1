4ead7088a2c189d6a3db1d28786b86f7f1121ccb
weite122 <949193531@qq.com>
2017-08-06 03:17:50 +0800

    点击注册后使用 LeanCloud API 注册

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -1,26 +1,31 @@
 import AV from 'leancloud-storage'
 
-var APP_ID = 'sLXuddPNQXQva9WI4rUTadNI-gzGzoHsz';
-var APP_KEY = 'd1abvnyhczW83URwerCRa8vk';
-
+var APP_ID = 'rUcmyykR2Tsi5VOPjfnkM7Yj-gzGzoHsz'
+var APP_KEY = 'Oy9HGah8eknBB6yTb9LOKjrt'
 AV.init({
   appId: APP_ID,
   appKey: APP_KEY
-});
+})
 
 export default AV
 
-export function signUp(username,password,successFn,errorFn){
+export function signUp(username, password, successFn, errorFn){
+   // 新建 AVUser 对象实例
   var user = new AV.User()
+  // 设置用户名
   user.setUsername(username)
+  // 设置密码
   user.setPassword(password)
-  user.signUp().then(function(loginedUser){
+  // 设置邮箱
+  user.signUp().then(function (loginedUser) {
     let user = getUserFromAVUser(loginedUser)
-    successFn.call(null,user)
-  },function (error){
-    errorFn.call(null,error)
+    successFn.call(null, user)
+  }, function (error) {
+    errorFn.call(null, error)
   })
+
   return undefined
+
 }
 
 function getUserFromAVUser(AVUser){
