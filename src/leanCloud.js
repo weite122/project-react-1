b0d64fc949aba5073e849d9c48a8c43196448785
weite122 <949193531@qq.com>
2017-08-06 03:03:54 +0800

    点击注册后使用 LeanCloud API 注册

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -9,3 +9,23 @@ AV.init({
 });
 
 export default AV
+
+export function signUp(username,password,successFn,errorFn){
+  var user = new AV.User()
+  user.setUsername(username)
+  user.setPassword(password)
+  user.signUp().then(function(loginedUser){
+    let user = getUserFromAVUser(loginedUser)
+    successFn.call(null,user)
+  },function (error){
+    errorFn.call(null,error)
+  })
+  return undefined
+}
+
+function getUserFromAVUser(AVUser){
+  return {
+    id: AVUser.id,
+    ...AVUser.attributes
+  }
+}
