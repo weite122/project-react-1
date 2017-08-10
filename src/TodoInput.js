9835429568dbaeac0f2c5de01bc252fad5937862
weite122 <949193531@qq.com>
2017-08-11 03:41:37 +0800

    将TodoInput变成函数

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,20 +1,18 @@
-import React,{ Component } from 'react';
+import React, {Component} from 'react';
 import './TodoInput.css'
 
-export default class TodoInput extends Component{
-    render(){
-        return <input type="text" value={this.props.content} 
-            className="TodoInput"
-            onChange={this.changeTitle.bind(this)}
-            onKeyPress={this.submit.bind(this)}/>
-    }
-    submit(e){
-        if(e.key === 'Enter'){
-            console.log('用户按回车了');
-            this.props.onSubmit(e)
-        }
-    }
-    changeTitle(e){
-        this.props.onChange(e)
-    }
+function submit (props, e) {
+  if (e.key === 'Enter') {
+    props.onSubmit(e)
+  }
+}
+function changeTitle (props, e) {
+  props.onChange(e)
+}
+
+export default function (props) {
+  return <input type="text" value={props.content}
+    className="TodoInput"
+    onChange={changeTitle.bind(null, props)}
+    onKeyPress={submit.bind(null, props)}/>
 }
